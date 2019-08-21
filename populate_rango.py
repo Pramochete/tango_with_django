bc2f6f422392f568432388feedd567c3422538bc
Pramochete <orientedresearch@gmail.com>
2019-08-21 19:47:51 +0545

    solved adding views adn likes exercise but not customizing admin

--- populate_rango.py
+++ populate_rango.py
@@ -38,9 +38,16 @@ def populate():
          "url":"http://flask.pocoo.org"}
     ]
 
-    cats = {"Python":{"pages":python_pages},
-            "Django":{"pages":django_pages},
-            "Other Frameworks":{"pages":other_pages},}
+    cats = {"Python":{"pages":python_pages,
+                      "views":128,
+                      "likes":64},
+            "Django":{"pages":django_pages,
+                      "views":64,
+                      "likes":32},
+            "Other Frameworks":{"pages":other_pages,
+                                "views":64,
+                                "likes":32},
+            }
     # If you want to add more categories or pages,
     # add them to the dictionaries above
 
@@ -49,7 +56,7 @@ def populate():
     # If you are using python 2.0x then cats.iteitems()
 
     for cat, cat_data in cats.items():
-        c= add_cat(cat)
+        c= add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c,p["title"],p["url"])
     
@@ -65,8 +72,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c= Category.objects.get_or_create(name=name)[0]
+    c.views= views
+    c.likes=likes
     c.save()
     return c
 

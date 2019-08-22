b9bd53361b23352648a50fb11a3e76089278614a
Pramochete <orientedresearch@gmail.com>
2019-08-22 22:25:56 +0545

    created app_name

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('views/', views.views, name='views'),

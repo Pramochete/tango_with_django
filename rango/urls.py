544500104fd262f8d096d23f4614f0e82eb4a336
Pramochete <orientedresearch@gmail.com>
2019-08-22 17:13:37 +0545

    made page of each category displayed

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('views/', views.views, name='views'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    re_path(r'^category/(?P<category_name_url>[\w\-]+)/$',views.show_category,name='show_category')
 ]

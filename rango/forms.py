dbd08385d1025ef2213604cb18cd76c3cef9801d
Pramochete <orientedresearch@gmail.com>
2019-08-25 11:35:12 +0545

    created register view and template

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -44,3 +45,15 @@ class PageForm(forms.ModelForm):
 
             return cleaned_data
             
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    # Anything within a nested Meta class describes additional properties about the particular class to which it belongs.
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')

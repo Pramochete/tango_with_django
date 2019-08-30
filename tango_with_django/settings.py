f6e4b00b40e7648ab24e60cbbe429a4dc7dad594
Pramochete <orientedresearch@gmail.com>
2019-08-30 22:06:11 +0545

    setting django-registration-redux

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,18 @@ AUTH_PASSWORD_VALIDATORS = [
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # SESSION_COOKIE_AGE = 1209600 will mean your webiste's cookies expire after a two week period
+
+# If True, users can register
+REGISTRATION_OPEN=True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS=7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN=True
+# The page you want users to arrive at after they successfully log in
+LOGIN_REDIRECT_URL='/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL='/accounts/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls'))
+    path('rango/', include('rango.urls')),
+    path('accounts/',include('registration.backends.simple.urls'))
 ] + static(
     settings.MEDIA_URL, 
     document_root=settings.MEDIA_ROOT
